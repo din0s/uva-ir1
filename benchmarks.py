1934d6fdf3eb3bcdde331554bd8522b9532b505a
ObviouslyLuuk <54840183+ObviouslyLuuk@users.noreply.github.com>
2022-02-28 18:00:58 +0100

    added k=20

--- benchmarks.py
+++ benchmarks.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
     assert results['2']['TF-IDF']['Precision@10'] >= 0.0969
 
     # TODO: We do not have recall@20 and precision@20.
-    # assert results['2']['NaiveQL']['Recall@20'] >= 0.0130
-    # assert results['2']['NaiveQL']['Precision@20'] >= 0.0086
+    assert results['2']['NaiveQL']['Recall@20'] >= 0.0130
+    assert results['2']['NaiveQL']['Precision@20'] >= 0.0086
 
     assert results['2']['QL']['Recall@5'] >= 0.1211
     assert results['2']['QL']['Precision@5'] >= 0.2261
